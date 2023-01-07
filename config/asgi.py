0020846345b28809e1b7212a8f1efd9501179281
minu <brotheratoz3@naver.com>
2023-01-07 19:23:39 +0900

    feat: create a simple websocket server

--- config/asgi.py
+++ config/asgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+import app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -18,4 +20,8 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+
+    "websocket": URLRouter(
+        app.routing.websocket_urlpatterns
+    ),
 })
