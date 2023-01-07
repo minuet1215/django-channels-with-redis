c46cdcc153c84cce8b62674dcad6c040509be099
minu <brotheratoz3@naver.com>
2023-01-07 21:32:46 +0900

    feat: js 객체 직렬화, 역직렬화

--- app/consumer.py
+++ app/consumer.py
@@ -1,7 +1,17 @@
+import json
+
 from channels.generic.websocket import WebsocketConsumer
 
 
 class EchoConsumer(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
-        self.send(f"You said: {text_data}")
+        obj = json.loads(text_data)
+        print("received: ", obj)
+
+        json_string: str = json.dumps({
+            "content": obj["content"],
+            "user": obj["user"]
+        })
+        # self.send(f"You said: {text_data}")
+        self.send(json_string)
