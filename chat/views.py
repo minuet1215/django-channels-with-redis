3f8241e4a8f46b0c0a7004044be8d0934fcc6344
minu <brotheratoz3@naver.com>
2023-01-08 20:21:38 +0900

    feat: lobby page, chatting ui 구현

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "chat/index.html")
+
+
+def room_chat(request: HttpRequest, room_name: str) -> HttpResponse:
+    return render(request, "chat/room_chat.html", {
+        "room_name": room_name,
+    })
