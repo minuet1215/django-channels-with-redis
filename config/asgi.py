12d99ad442903fd42522025c52ab82ef324277ec
minu <brotheratoz3@naver.com>
2023-01-09 16:32:15 +0900

    feat: chat server routing 추가

--- config/asgi.py
+++ config/asgi.py
@@ -13,6 +13,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 import app.routing
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -22,6 +23,7 @@ application = ProtocolTypeRouter({
     "http": django_asgi_app,
 
     "websocket": URLRouter(
-        app.routing.websocket_urlpatterns
+        app.routing.websocket_urlpatterns,
+        chat.routing.websocket_urlpatterns,
     ),
 })
