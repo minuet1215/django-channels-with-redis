773f7b5f377ce1b2a9972a0f56f2185030b142cb
minu <brotheratoz3@naver.com>
2023-01-10 18:03:35 +0900

    feat: 로그인하지 않은 유저는 채팅방에 입장하지 못하는 인증 기능 구현

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpRequest, HttpResponse
+
 from chat.forms import RoomForm
 from chat.models import Room
 
@@ -12,6 +14,7 @@ def index(request: HttpRequest) -> HttpResponse:
     })
 
 
+@login_required
 def room_chat(request, room_pk):
     room = get_object_or_404(Room, pk=room_pk)
     return render(request, "chat/room_chat.html", {
@@ -19,6 +22,7 @@ def room_chat(request, room_pk):
     })
 
 
+@login_required
 def room_new(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
@@ -31,3 +35,14 @@ def room_new(request):
     return render(request, "chat/room_form.html", {
         "form": form,
     })
+
+# class RoomCreateView(LoginRequiredMixin, CreateView):
+#     form_class = RoomForm
+#     template_name = "chat/room_form.html"
+#
+#     def get_success_url(self):
+#         created_room = self.object
+#         return resolve_url("chat:room_chat", created_room.pk)
+#
+#
+# room_new = RoomCreateView.as_view()
