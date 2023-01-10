b8d16878ffb20a70a2ab8beeb814abe0f7ac06da
minu <brotheratoz3@naver.com>
2023-01-10 17:39:28 +0900

    feat: 로그인, 로그아웃 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LogoutView, LoginView
 
 # Create your views here.
+login = LoginView.as_view(
+    template_name='partials/form.html',
+    extra_context={
+        'form_name': "login",
+        'submit_label': 'login',
+    }
+)
+
+logout = LogoutView.as_view(
+    next_page='accounts:login',
+)
